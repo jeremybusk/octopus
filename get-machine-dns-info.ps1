16a60c9eb6b71bd99dc6d0816d5ad73655f420c1
Jeremy Busk <jeremybusk@gmail.com>
2021-02-08 15:26:22 -0700

    Create get-machine-dns-info.ps1

---
+++ get-machine-dns-info.ps1
@@ -0,0 +1,34 @@
+#!/usr/bin/env pwsh
+# Get machines that belong to a role and get dns info on them
+# This was actually used built on pwsh on Linux
+
+$ErrorActionPreference = "Stop";
+
+$octopusURL = "<YOURURL>"
+$octopusAPIKey = "<YOURAPIKEY>"
+$header = @{ "X-Octopus-ApiKey" = $octopusAPIKey }
+$spaceName = "<YOURSPACE>"
+$userRoleName = "<YOURROLE>"
+
+$userRole = (Invoke-RestMethod -Method Get -Uri "$octopusURL/api/userroles/all" -Headers $header) | Where-Object {$_.Name -eq $userRoleName}
+
+$machines = Invoke-RestMethod -Method Get -Uri "$octopusURL/api/$($space.Id)/machines/all" -Headers $header
+
+$machineNames = @()
+foreach ($machine in $machines)
+{
+    if ( $machine | Where-Object Roles -contains IFP){
+        write-host "================================================="
+        write-host $machine.Name
+        $url = $machine.Uri
+        $fqdn = ($url).split(':')[1] -replace "/",''
+        write-host "host fqdn: $fqdn"
+        write-host "url: $url"
+        $forward = $(host $fqdn)
+        write-host "forward: $forward"
+        $ip = $(dig +short $fqdn)
+        write-host "ip: $ip"
+        $names=$(dig +short -x $ip)  # reverse dns names (ptrs)
+        write-host "reverse: $names"
+    }
+}
